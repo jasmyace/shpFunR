c58cc6f4b3b31abc2c0741d28b814c16dafe78fa
jasmyace <jasmyace@gmail.com>
2017-03-18 20:20:17 -0600

    Updates to allow for different class types and assignment of ID variable.

--- makeSquare.R
+++ makeSquare.R
@@ -1,13 +1,30 @@
-makeSquare <- function(p1,p2,p3,p4){
+makeSquare <- function(p1,p2,p3,p4,class="q",id=NULL){
   
   coords <- rbind(p1,p2,p3,p4,p1)
   
   df <- data.frame(X=1,row.names="1")
   
-  c1 <- Polygon(coords)
-  c2 <- Polygons(list(c1),ID="1")
-  c3 <- SpatialPolygons(list(c2))
-  c4 <- SpatialPolygonsDataFrame(c3,df)
-  return(c4)
+  if(class=="p"){
+    c1 <- SpatialPoints(coords)
+    rt <- c1
+  } else if(class=="l"){
+    c1 <- Line(coords)
+    c2 <- Lines(list(c1),ID="1")
+    c3 <- SpatialLines(list(c2))
+    c4 <- SpatialLinesDataFrame(c3,df)
+    rt <- c4
+  } else if(class=="q"){
+    c1 <- Polygon(coords)
+    c2 <- Polygons(list(c1),ID="1")
+    c3 <- SpatialPolygons(list(c2))
+    c4 <- SpatialPolygonsDataFrame(c3,df)
+    rt <- c4
+  } else {
+    stop("Invalid class specified;  try again with one of 'p', 'l', or 'q'.")
+  }
   
+  if(length(id) != 1 & !is.null(id)) stop("When specified, the length of argument id should be 1.")
+  if(!is.null(id)) rt <- spChFIDs(rt, as.character(id))
+  
+  return(rt)
 }
